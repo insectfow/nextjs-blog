a5a89e7013e9b69ea2654d58f98133c6f776aaa1
insectfow <ehdgus441@naver.com>
2022-12-07 16:49:30 +0900

    next/head and next/script study!

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 2. next/link study!
 
 3. next/image and static file serving study!
+
+4. next/head and next/script study!
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,17 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 import { ImageComponent } from '../../public/ImageComponent';
 const FirstPost = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+        <Script src="https://connect.facebook.net/en_US/sdk.js"
+          strategy='lazyOnload'
+          onLoad={console.log('loaded')}
+        />
+      </Head>
       <h1>First Post</h1>
       {ImageComponent()}
       <h2>
