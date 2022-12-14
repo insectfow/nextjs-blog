216fbb5b7a7150fcc4c4b58cf6eadaa3dac34cca
insectfow <ehdgus441@naver.com>
2022-12-14 18:31:13 +0900

    Dynamic Routes update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/laylout.js';
 import utilStyles from '../styles/utils.module.scss';
+import Link from 'next/link';
+import Date from '../components/date';
 
 import { getSortedPostsData } from '../lib/index';
 
@@ -20,16 +22,16 @@ export default function Home({allPostsData} ){
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <h1>Home</h1>
+        <h1>Blog</h1>
         <ul>
           { allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,9 @@
 import Layout from '../../components/laylout';
-import { getAllPostIds, getPostData  } from '../../lib/index';
+import { getAllPostIds, getPostData } from '../../lib/index';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.scss';
+
 export async function getStaticPaths() {
   // Return a list of possible value for id
   const paths = getAllPostIds();
@@ -11,7 +15,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -22,13 +26,18 @@ export async function getStaticProps({ params }) {
 
 const Post = ({postData}) => {
   return (
-  <Layout Post>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-  </Layout>
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
   )
 }
 
